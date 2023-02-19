c07d8fad04e3badf9dda980d44482e87f2e5b08e
Aditya Nair <adinair.in@gmail.com>
2023-02-19 16:57:43 -0600

    Updated webpack.config.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -38,6 +38,9 @@ registerRoute(
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
     ],
   })
 );
